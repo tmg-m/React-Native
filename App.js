79c4c783791b73349f192536b0ef45509f8c9c75
Mahendra <bus.mahendrat@gmail.com>
2022-11-01 00:42:00 +0100

    added style to new button

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Pressable } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.innerText}>My first React Native App</Text>
       <StatusBar style="auto" />
-      <TextInput style={styles.myText}>Type Here</TextInput>
-      <Button style={styles.myBtn} title='Button'/>
+
+      <TextInput style={styles.myInputText}>Type Here</TextInput>
+
+      <Button title='Button (cannot use styles)'/>
+
+      <Pressable style={styles.myBtn}>
+        <Text style={styles.textNorm}>Button wiht styles</Text>
+      </Pressable>
+
     </View>
   );
 }
@@ -22,12 +29,24 @@ const styles = StyleSheet.create({
     borderColor: 'orange',
     borderRadius:-20,
   },
-  myText: {
+  myInputText: {
     paddingVertical:10,
     paddingHorizontal: 50,
     marginVertical:10,
     borderWidth: '2',
     borderRadius: '8',
     borderColor: 'purple',
+  },
+  myBtn: {
+    paddingVertical:10,
+    paddingHorizontal: 50,
+    marginVertical:10,
+    backgroundColor: 'black',
+    borderWidth: '2',
+    borderRadius: '8',
+    borderColor: 'red',
+  },
+  textNorm : {
+    color: 'white',
   },
 });
