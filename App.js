4a1884369228d8016fb50efe238394125d9ea785
Mahendra <bus.mahendrat@gmail.com>
2022-11-01 00:15:38 +0100

    text input & button added with basic styles

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My first React Native App</Text>
+      <Text style={styles.innerText}>My first React Native App</Text>
       <StatusBar style="auto" />
+      <TextInput style={styles.myText}>Type Here</TextInput>
+      <Button style={styles.myBtn} title='Button'/>
     </View>
   );
 }
@@ -16,5 +18,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    borderWidth: 10,
+    borderColor: 'orange',
+    borderRadius:-20,
+  },
+  myText: {
+    paddingVertical:10,
+    paddingHorizontal: 50,
+    marginVertical:10,
+    borderWidth: '2',
+    borderRadius: '8',
+    borderColor: 'purple',
   },
 });
